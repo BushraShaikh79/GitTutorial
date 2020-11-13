216b3168091dd90944f003c6bc01808d0215c651
Bushra Shaikh <skbushrar@gmail.com>
2020-11-13 19:39:11 +0530

    Added new feature

---
+++ GitTutorial/NewFeature.swift
@@ -0,0 +1,18 @@
+//
+//  NewFeature.swift
+//  GitTutorial
+//
+//  Created by JustMac on 13/11/20.
+//
+
+import UIKit
+import MapKit
+
+class NewFeature: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("NewFeature")
+    }
+}
