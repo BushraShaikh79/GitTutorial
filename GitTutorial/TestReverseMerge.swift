68e344523f59799712c1a10c589513aaae28d616
Bushra Shaikh <skbushrar@gmail.com>
2020-11-24 12:07:36 +0530

    Testing reverse merge

---
+++ GitTutorial/TestReverseMerge.swift
@@ -0,0 +1,14 @@
+//
+//  TestMerge.swift
+//  GitTutorial
+//
+//  Created by JustMac on 24/11/20.
+//
+
+import Foundation
+
+class TestReverseMerge {
+    
+    //This file is to test reverse merge..
+    //means we will try to merge main branch in our current sub branch (i.e New Feature).. so that we will get the source of our branch with main branch source without updating the main branch
+}
